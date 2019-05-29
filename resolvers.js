9288deba4b581d9f8d2601a43a80c72544f2445c
pedropmedina <pedrodelilia@gmail.com>
2019-05-28 23:22:06 -0400

    Add mutation createPin

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,7 @@
 const { AuthenticationError } = require('apollo-server');
 
+const Pin = require('./models/Pin');
+
 const authenticated = next => (root, args, ctx, info) => {
   if (!ctx.currentUser) {
     throw new AuthenticationError('You must be logged in');
@@ -10,5 +12,15 @@ const authenticated = next => (root, args, ctx, info) => {
 module.exports = {
   Query: {
     me: authenticated((root, args, ctx) => ctx.currentUser)
+  },
+  Mutation: {
+    createPin: authenticated(async (root, args, ctx) => {
+      const newPin = await new Pin({
+        ...args.input,
+        author: ctx.currentUser._id
+      }).save();
+      const pinAdded = await Pin.populate(newPin, 'author');
+      return pinAdded;
+    })
   }
 };
